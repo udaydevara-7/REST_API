be1d46ff5c88c2d8f59761b829c96575a2c8b6ab
Udaykiran Devara <161283768+udaydevara-7@users.noreply.github.com>
2024-05-22 14:54:36 +0530

    Add files via upload

---
+++ REST_API/index.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+const routes = require('./routes/routes');
+const express = require('express');
+const Model = require('./model/model');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', routes)
+
+
+//routes
+//get method
+app.get('/', (req, res) => {
+    res.send('Hello, World!');            //Hello, world!
+});
+
+//Post Method
+// routes.post('/post', (req, res) => {
+//     res.send('Post API')
+// })
+
+// Get by ID Method
+routes.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Posting the data to database using db model
+routes.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+//Get all method
+routes.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+routes.get('/getOne/:id/:name', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update by ID Method
+routes.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Delete by ID Method
+routes.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
